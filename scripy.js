46d066cd9b265738f32355cfca608b5adff27245
a-soliman <ahmed.soliman@programmer.net>
2018-06-25 19:11:42 -0700

    isUnique

---
+++ scripy.js
@@ -0,0 +1,12 @@
+/*
+
+Is Unique
+
+Instructions
+Create a function that determines whether all chars in a string are unique or not., make it case senstive.
+
+Input: String,
+Output: Boolean
+
+*/
+
